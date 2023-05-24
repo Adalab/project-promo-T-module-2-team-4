07091a785f00d125ac1aac4637b60d421a6f296d
Silvia Parada <silviaparadag@yahoo.es>
2023-05-24 20:26:09 +0200

    Correct error

--- src/js/01_form.js
+++ src/js/01_form.js
@@ -10,30 +10,29 @@ const shareLegend = document.querySelector('.js__share');
 const shareContainer = document.querySelector('.js__share__container');
 
 const shareContainer2 = document.querySelector('.js__share__container2');
-//const shareBorder3 = document.querySelector('.js__share__border3'); 
+//const shareBorder3 = document.querySelector('.js__share__border3');
 
-designLegend.addEventListener('click', (event) => {
- //fillContainer.classList.add('collapsed');
+designLegend.addEventListener('click', () => {
   shareContainer.classList.add('collapsed');
   shareContainer2.classList.add('collapsed');
- // shareBorder3.classList.add('collapsed');
   designContainer.classList.remove('collapsed');
 });
+//fillContainer.classList.add('collapsed');
+//shareBorder3.classList.add('collapsed');
 
-fillLegend.addEventListener('click', (event) => {
+fillLegend.addEventListener('click', () => {
   designContainer.classList.add('collapsed');
   shareContainer.classList.add('collapsed');
   shareContainer2.classList.add('collapsed');
- // shareBorder3.classList.add('collapsed');
   fillContainer.classList.remove('collapsed');
 });
+// shareBorder3.classList.add('collapsed');
 
-shareLegend.addEventListener('click', (event) =>{
- // shareContainer2.classList.add('collapsed');
+shareLegend.addEventListener('click', () =>{
   designContainer.classList.add('collapsed');
   fillContainer.classList.add('collapsed');
   shareContainer.classList.remove('collapsed');
-
 });
+// shareContainer2.classList.add('collapsed');
 
 

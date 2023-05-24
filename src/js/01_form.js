b2e4fde51dc5f836d9a4ddd3ac30b86655a81c22
Giselle-97 <giselle.cduran@hotmail.com>
2023-05-24 20:22:37 +0200

    added event js

--- src/js/01_form.js
+++ src/js/01_form.js
@@ -10,13 +10,13 @@ const shareLegend = document.querySelector('.js__share');
 const shareContainer = document.querySelector('.js__share__container');
 
 const shareContainer2 = document.querySelector('.js__share__container2');
-const shareBorder3 = document.querySelector('.js__share__border3'); 
+//const shareBorder3 = document.querySelector('.js__share__border3'); 
 
 designLegend.addEventListener('click', (event) => {
-  fillContainer.classList.add('collapsed');
+ //fillContainer.classList.add('collapsed');
   shareContainer.classList.add('collapsed');
   shareContainer2.classList.add('collapsed');
-  shareBorder3.classList.add('collapsed');
+ // shareBorder3.classList.add('collapsed');
   designContainer.classList.remove('collapsed');
 });
 
@@ -24,33 +24,16 @@ fillLegend.addEventListener('click', (event) => {
   designContainer.classList.add('collapsed');
   shareContainer.classList.add('collapsed');
   shareContainer2.classList.add('collapsed');
-  shareBorder3.classList.add('collapsed');
+ // shareBorder3.classList.add('collapsed');
   fillContainer.classList.remove('collapsed');
 });
 
 shareLegend.addEventListener('click', (event) =>{
-  shareContainer2.classList.add('collapsed');
+ // shareContainer2.classList.add('collapsed');
   designContainer.classList.add('collapsed');
   fillContainer.classList.add('collapsed');
   shareContainer.classList.remove('collapsed');
 
 });
 
-// CLASE DE DAYANA
 
-/* 1º llamar al input */
-
-const inputName = document.querySelector('.js_name');
-// js_name está en el partial de formfill.html
-
-const previewName = document.querySelector('.js__preview_name');
-// js_preview_name clase de Dayana para el Nombre y Apellidos del preview
-
-function handlerInputName(event) {
-  const inputValue = event.target.value;
-  previewName.innerHTML = inputValue;
-}
-
-inputName.addEventListener('input', handlerInputName);
-
-//Sería así con todos los demás, profesión, links SM, etc.

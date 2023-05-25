23e4f2460eb4fa1f911e19615d95443a3df57f0f
SuelenGalhardo <suelenranucci@gmail.com>
2023-05-25 16:52:22 +0200

    add js preview

--- src/js/03_design.js
+++ src/js/03_design.js
@@ -2,18 +2,18 @@
 
 const radio1 = document.querySelector('.js_radio-1');
 
-const radio2 = document.querySelector('js_radio-2');
+const radio2 = document.querySelector('.js_radio-2');
 
-const radio3 = document.querySelector('js_radio-3');
+const radio3 = document.querySelector('.js_radio-3');
 
-const radio4 = document.querySelector('js_radio-4');
+const radio4 = document.querySelector('.js_radio-4');
 
-const previewCard = document.querySelector('js__preview');
+const previewCard = document.querySelector('.js__preview');
 // js__preview parte Preview
 
 
 function handleClickRadio1() {
-  previewCard.classList.remove('palette-2', 'palette-3','palette-4');
+  previewCard.classList.remove('palette-2','palette-3','palette-4');
   previewCard.classList.add('palette-1');
 }
 function handleClickRadio2() {
@@ -26,7 +26,7 @@ function handleClickRadio3() {
 }
 
 function handleClickRadio4() {
-  previewCard.classList.remove('palette-1', 'palette-2' ,'palette-3');
+  previewCard.classList.remove('palette-1', 'palette-2','palette-3');
   previewCard.classList.add('palette-4');
 }
 
---
+++ src/js/04_fill.js
@@ -0,0 +1,17 @@
+'use strict';
+const inputName  = document.querySelector ('.js__name');
+const previewName = document.querySelector('.js__preview_name');
+const inputJob = document.querySelector('.js__job');
+const previewJob = document.querySelector('.js__description');
+const inputeMail = document.querySelector('.js__email');
+
+
+function handlerInputName(event) { const inputValue = event.target.value;
+  previewName.innerHTML = inputValue;
+}
+function handlerInputJob(event) { const inputValue = event.target.value;
+  previewJob.innerHTML = inputValue;
+}
+
+inputName.addEventListener('input', handlerInputName);
+inputJob.addEventListener('input', handlerInputJob);

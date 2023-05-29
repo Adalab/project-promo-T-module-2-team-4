3b869f2682b147e87fafd062802041a8de33045e
Silvia Parada <silviaparadag@yahoo.es>
2023-05-29 12:49:46 +0200

    Simplify form inputs

--- src/js/04_fill.js
+++ src/js/04_fill.js
@@ -1,55 +1,53 @@
 'use strict';
-const inputName  = document.querySelector ('.js__name');
+
+//Querys
+//const inputName  = document.querySelector ('.js__name');
 const previewName = document.querySelector('.js__preview_name');
 
-const inputJob = document.querySelector('.js__job');
+//const inputJob = document.querySelector('.js__job');
 const previewJob = document.querySelector('.js__description');
 
-const inputMail = document.querySelector('.js__email');
+//const inputMail = document.querySelector('.js__email');
 const previewMail = document.querySelector('.js__preview_mail');
 
-const inputTelephone = document.querySelector('.js__telephone');
+//const inputTelephone = document.querySelector('.js__telephone');
 const previewTelephone = document.querySelector('.js__preview_telephone');
 
-const inputLinkedIn = document.querySelector('.js__linkedin');
+//const inputLinkedIn = document.querySelector('.js__linkedin');
 const previewLinkedIn = document.querySelector('.js__preview_linkedin');
 
-const inputGitHub = document.querySelector('.js__github');
+//const inputGitHub = document.querySelector('.js__github');
 const previewGitHub = document.querySelector('.js__preview_github');
 
-function handlerInputName(event) {
-  const inputValue = event.target.value;
-  previewName.innerHTML = inputValue;
-}
-function handlerInputJob(event) {
-  const inputValue = event.target.value;
-  previewJob.innerHTML = inputValue;
+const fillInput =document.querySelector('.js__fill');
+
+const data = {
+  palete: 1,
+  full_name: '',
+  job: '',
+  photo: '',
+  email: '',
+  phone: '',
+  linkedin: '',
+  github: '',
+};
+
+// Funciones
+function renderPreview() {
+  previewName.innerHTML = data.full_name;
+  previewJob.innerHTML = data.job;
+  previewMail.href = `mailto:${data.email}`;
+  previewTelephone.href = `tel:${data.phone}`;
+  previewLinkedIn.href = data.linkedin;
+  previewGitHub.href = `https://github.com/${data.github.replace('@', '')}`;
 }
 
-function handlerInputMail(event) {
-  const inputMail = event.target.value;
-  previewMail.href = `mailto:${inputMail}`;
+function handlerInputs (event) {
+  const idInput = event.target.id;
+  const valueInput = event.target.value;
+
+  data[idInput] = valueInput;
+  renderPreview();
 }
 
-function handlerInputTelephone(event) {
-  const inputTelephone = event.target.value;
-  previewTelephone.href = `tel:${inputTelephone}`;
-}
-
-function handlerInputLinkedIn(event) {
-  const inputLinkedIn = event.target.value;
-  previewLinkedIn.href = inputLinkedIn;
-}
-
-function handlerInputGitHub(event) {
-  const inputGitHub = event.target.value;
-  console.log(inputGitHub);
-  previewGitHub.href = `https://github.com/${inputGitHub.replace('@', '')}`;
-}
-
-inputName.addEventListener('input', handlerInputName);
-inputJob.addEventListener('input', handlerInputJob);
-inputMail.addEventListener('input', handlerInputMail);
-inputTelephone.addEventListener('input', handlerInputTelephone);
-inputLinkedIn.addEventListener('input', handlerInputLinkedIn);
-inputGitHub.addEventListener('input', handlerInputGitHub);
+fillInput.addEventListener('keyup', handlerInputs);

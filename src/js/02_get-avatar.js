66629307648dd42cc76817843906a683c2d0aa14
Silvia Parada <silviaparadag@yahoo.es>
2023-05-30 19:10:33 +0200

    Remove comments

--- src/js/02_get-avatar.js
+++ src/js/02_get-avatar.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const fr = new FileReader();
-const fileField = document.querySelector('.js__profile-upload-btn'); //linea 18 formfill
-const fileField2 = document.querySelector('.js__profile-upload-btn2'); //linea 20 formfill
-const profilePreview = document.querySelector('.js__profile-preview'); //linea 12 preview en el preview G
-const redPreview = document.querySelector('.js__red-preview'); //linea24 formfill, imagen reducida
+const fileField = document.querySelector('.js__profile-upload-btn');
+const fileField2 = document.querySelector('.js__profile-upload-btn2');
+const profilePreview = document.querySelector('.js__profile-preview');
+const redPreview = document.querySelector('.js__red-preview');
 
 function getImage(e) {
   const myFile = e.currentTarget.files[0];
--- src/js/04_fill.js
+++ src/js/04_fill.js
@@ -1,29 +1,22 @@
 'use strict';
 
-//Querys
-//const inputName  = document.querySelector ('.js__name');
 const previewName = document.querySelector('.js__preview_name');
 
-//const inputJob = document.querySelector('.js__job');
 const previewJob = document.querySelector('.js__description');
 
-//const inputMail = document.querySelector('.js__email');
 const previewMail = document.querySelector('.js__preview_mail');
 
-//const inputTelephone = document.querySelector('.js__telephone');
 const previewTelephone = document.querySelector('.js__preview_telephone');
 
-//const inputLinkedIn = document.querySelector('.js__linkedin');
 const previewLinkedIn = document.querySelector('.js__preview_linkedin');
 
-//const inputGitHub = document.querySelector('.js__github');
 const previewGitHub = document.querySelector('.js__preview_github');
 
 const fillInput =document.querySelector('.js__fill');
 
 const data = {
   palete: 1,
-  full_name: '',
+  fullName: '',
   job: '',
   photo: '',
   email: '',
@@ -32,9 +25,9 @@ const data = {
   github: '',
 };
 
-// Funciones
+
 function renderPreview() {
-  previewName.innerHTML = data.full_name;
+  previewName.innerHTML = data.fullName;
   previewJob.innerHTML = data.job;
   previewMail.href = `mailto:${data.email}`;
   previewTelephone.href = `tel:${data.phone}`;

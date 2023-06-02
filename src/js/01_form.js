740b6ce3d2b4c1117d9d5d9656ea1460ce25bb79
kathimansilla <mansillakatherina@gmail.com>
2023-06-02 12:19:57 +0200

    local changes

--- src/js/01_form.js
+++ src/js/01_form.js
@@ -1,53 +1,52 @@
-'use strict';
+"use strict";
 
-const arrowRotate1 = document.querySelector('.js_arrow-1');
-const arrowRotate2 = document.querySelector('.js_arrow-2');
-const arrowRotate3 = document.querySelector('.js_arrow-3');
-const designLegend = document.querySelector('.js__design');
-const designContainer = document.querySelector('.js__design__container');
+const arrowRotate1 = document.querySelector(".js_arrow-1");
+const arrowRotate2 = document.querySelector(".js_arrow-2");
+const arrowRotate3 = document.querySelector(".js_arrow-3");
+const designLegend = document.querySelector(".js__design");
+const designContainer = document.querySelector(".js__design__container");
 
-const fillLegend = document.querySelector('.js__fill');
-const fillContainer = document.querySelector('.js__fill__container');
+const fillLegend = document.querySelector(".js__fill");
+const fillContainer = document.querySelector(".js__fill__container");
 
-const shareLegend = document.querySelector('.js__share');
-const shareContainer = document.querySelector('.js__share__container');
+const shareLegend = document.querySelector(".js__share");
+const shareContainer = document.querySelector(".js__share__container");
 
-const shareContainer2 = document.querySelector('.js__share__container2');
+const shareContainer2 = document.querySelector(".js__share__container2");
 //const shareBorder3 = document.querySelector('.js__share__border3');
 
-designLegend.addEventListener('click', (event) => {
+designLegend.addEventListener("click", (event) => {
   //fillContainer.classList.add('collapsed');
-  shareContainer.classList.add('collapsed');
-  shareContainer2.classList.add('collapsed');
+  shareContainer.classList.add("collapsed");
+  shareContainer2.classList.add("collapsed");
   // shareBorder3.classList.add('collapsed');
-  designContainer.classList.remove('collapsed');
-  fillContainer.classList.add('collapsed');
-  arrowRotate1.classList.add('rotate');
-  arrowRotate2.classList.remove('rotate');
-  arrowRotate3.classList.remove('rotate');
+  designContainer.classList.remove("collapsed");
+  fillContainer.classList.add("collapsed");
+  arrowRotate1.classList.add("rotate");
+  arrowRotate2.classList.remove("rotate");
+  arrowRotate3.classList.remove("rotate");
 });
 //shareBorder3.classList.add('collapsed');
 
-fillLegend.addEventListener('click', () => {
-  designContainer.classList.add('collapsed');
-  shareContainer.classList.add('collapsed');
-  shareContainer2.classList.add('collapsed');
+fillLegend.addEventListener("click", () => {
+  designContainer.classList.add("collapsed");
+  shareContainer.classList.add("collapsed");
+  shareContainer2.classList.add("collapsed");
   // shareBorder3.classList.add('collapsed');
-  fillContainer.classList.remove('collapsed');
-  arrowRotate1.classList.remove('rotate');
-  arrowRotate2.classList.add('rotate');
-  arrowRotate3.classList.remove('rotate');
+  fillContainer.classList.remove("collapsed");
+  arrowRotate1.classList.remove("rotate");
+  arrowRotate2.classList.add("rotate");
+  arrowRotate3.classList.remove("rotate");
 });
 // shareBorder3.classList.add('collapsed');
 
-shareLegend.addEventListener('click', (event) =>{
+shareLegend.addEventListener("click", (event) => {
   // shareContainer2.classList.add('collapsed');
-  designContainer.classList.add('collapsed');
-  fillContainer.classList.add('collapsed');
-  shareContainer.classList.remove('collapsed');
-  arrowRotate1.classList.remove('rotate');
-  arrowRotate2.classList.remove('rotate');
-  arrowRotate3.classList.add('rotate');
+  designContainer.classList.add("collapsed");
+  fillContainer.classList.add("collapsed");
+  shareContainer.classList.remove("collapsed");
+  arrowRotate1.classList.remove("rotate");
+  arrowRotate2.classList.remove("rotate");
+  arrowRotate3.classList.add("rotate");
 });
 // shareContainer2.classList.add('collapsed');
-
--- src/js/03_design.js
+++ src/js/03_design.js
@@ -10,14 +10,12 @@ const radio4 = document.querySelector('.js_radio-4');
 
 const previewCard = document.querySelector('.js__preview');
 
-
-
 function handleClickRadio1() {
-  previewCard.classList.remove('palette-2','palette-3','palette-4');
+  previewCard.classList.remove('palette-2', 'palette-3', 'palette-4');
   previewCard.classList.add('palette-1');
 }
 function handleClickRadio2() {
-  previewCard.classList.remove('palette-1','palette-3','palette-4');
+  previewCard.classList.remove('palette-1', 'palette-3', 'palette-4');
   previewCard.classList.add('palette-2');
 }
 function handleClickRadio3() {
@@ -26,7 +24,7 @@ function handleClickRadio3() {
 }
 
 function handleClickRadio4() {
-  previewCard.classList.remove('palette-1', 'palette-2','palette-3');
+  previewCard.classList.remove('palette-1', 'palette-2', 'palette-3');
   previewCard.classList.add('palette-4');
 }
 

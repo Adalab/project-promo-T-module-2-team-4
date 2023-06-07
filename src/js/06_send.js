9b29791af0f9851e1d661db927e4fecb556de72c
kathimansilla <mansillakatherina@gmail.com>
2023-06-07 19:19:32 +0200

    added alert message

--- src/js/06_send.js
+++ src/js/06_send.js
@@ -3,27 +3,37 @@
 const linkUrl = document.querySelector('.js_url');
 const msgError = document.querySelector('.js_msg-error');
 const msgSuccess = document.querySelector('.js_success-msg');
+function msgAlert(message) {
+  msgError.innerHTML = message;
+}
 
 function handleClickCreateBtn(event) {
   event.preventDefault();
   console.log('se ha enviado');
   console.log(data);
-  fetch('https://dev.adalab.es/api/card/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      if (data.success) {
+
+  if (data.name === '') {
+    msgAlert('Ha olvidado completar el nombre');
+  } else if (data.job === '') {
+    msgAlert('Ha olvidado completar la profesión');
+  } else if (data.photo === '') {
+    msgAlert('Ha olvidado añadir la imagen');
+  } else if (data.email === '') {
+    msgAlert('Ha olvidado completar el correo electrónico');
+  } else {
+    fetch('https://dev.adalab.es/api/card/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
         linkUrl.innerHTML = data.cardURL;
         linkUrl.href = data.cardURL;
         msgSuccess.innerHTML = 'La tarjeta ha sido creada:';
-      } else {
-        msgError.innerHTML = 'Recuerda rellenar todos los campos';
-      }
-    });
+      });
+  }
 }
 
 shareButton.addEventListener('click', handleClickCreateBtn);

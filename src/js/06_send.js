69fe6938b06c8acd3fa33c4e20585b44e34b4917
Giselle-97 <giselle.cduran@hotmail.com>
2023-06-15 11:03:44 +0200

    added changes issue#49

--- src/js/06_send.js
+++ src/js/06_send.js
@@ -3,8 +3,10 @@
 const linkUrl = document.querySelector('.js_url');
 const msgError = document.querySelector('.js_msg-error');
 const msgSuccess = document.querySelector('.js_success-msg');
+const btnTwitter = document.querySelector('.js_btnTw');
 function msgAlert() {
-  msgError.innerHTML = '¡Atención! Ha olvidado completar algunos campos obligatorios*';
+  msgError.innerHTML =
+    '¡Atención! Ha olvidado completar algunos campos obligatorios*';
 }
 
 function handleClickCreateBtn(event) {
@@ -15,21 +17,26 @@ function handleClickCreateBtn(event) {
   if (data.name === '') {
     nameInput.classList.add('borderRedEmpty');
     //message += ('el nombre, ');
-  } 
+  }
   if (data.job === '') {
     jobInput.classList.add('borderRedEmpty');
     //message += ('la profesión, ');
-  } 
+  }
   if (data.photo === '') {
     redPreview.classList.add('borderRedEmpty');
     //message += ('la imagen, ');
-  } 
+  }
   if (data.email === '') {
     emailInput.classList.add('borderRedEmpty');
     //message += ('el correo electrónico.');
-  } 
-  
-  if (data.name !== '' && data.job !== '' && data.photo !== '' && data.email !== '') {
+  }
+
+  if (
+    data.name !== '' &&
+    data.job !== '' &&
+    data.photo !== '' &&
+    data.email !== ''
+  ) {
     fetch('https://dev.adalab.es/api/card/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -40,10 +47,10 @@ function handleClickCreateBtn(event) {
         console.log(data);
         linkUrl.innerHTML = data.cardURL;
         linkUrl.href = data.cardURL;
+        btnTwitter.href = `https://twitter.com/intent/tweet?url=${linkUrl.href}&text=Ya%20estoy%20suscrita%20al%20Adalab%20Tech%20Summit%21%21%21&hashtags=Adalab`;
         msgSuccess.innerHTML = 'La tarjeta ha sido creada:';
       });
-  }
-  else {
+  } else {
     msgAlert();
     fillContainer.classList.remove('collapsed');
     shareContainer2.classList.add('collapsed');

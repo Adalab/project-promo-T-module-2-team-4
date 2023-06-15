b274cf66a1b2172ff358acfc77d2a3ef669fc89a
Giselle-97 <giselle.cduran@hotmail.com>
2023-06-15 13:53:24 +0200

    added local storage

--- src/js/06_send.js
+++ src/js/06_send.js
@@ -12,6 +12,34 @@ function msgAlert() {
 function msgAlert2() {
   msgError.innerHTML += ' El correo electrónico no es válido*';
 }
+const setLocalStorage = () => {
+  localStorage.setItem('infoCard', JSON.stringify(data));
+};
+const getLocalStorage = () => {
+  const dataCard = localStorage.getItem('infoCard');
+  if (dataCard) {
+    data = JSON.parse(dataCard);
+  }
+  renderPreview();
+  //writeImage();
+};
+getLocalStorage();
+
+const sendApiData = () => {
+  fetch('https://dev.adalab.es/api/card/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      linkUrl.innerHTML = data.cardURL;
+      linkUrl.href = data.cardURL;
+      btnTwitter.href = `https://twitter.com/intent/tweet?url=${linkUrl.href}&text=Ya%20estoy%20suscrita%20al%20Adalab%20Tech%20Summit%21%21%21&hashtags=Adalab`;
+      msgSuccess.innerHTML = 'La tarjeta ha sido creada:';
+    });
+};
 
 function handleClickCreateBtn(event) {
   event.preventDefault();
@@ -40,19 +68,7 @@ function handleClickCreateBtn(event) {
     data.photo !== '' &&
     data.email !== ''
   ) {
-    fetch('https://dev.adalab.es/api/card/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        linkUrl.innerHTML = data.cardURL;
-        linkUrl.href = data.cardURL;
-        btnTwitter.href = `https://twitter.com/intent/tweet?url=${linkUrl.href}&text=Ya%20estoy%20suscrita%20al%20Adalab%20Tech%20Summit%21%21%21&hashtags=Adalab`;
-        msgSuccess.innerHTML = 'La tarjeta ha sido creada:';
-      });
+    sendApiData();
   } else {
     msgAlert();
     fillContainer.classList.remove('collapsed');
@@ -62,6 +78,7 @@ function handleClickCreateBtn(event) {
     msgAlert2();
     console.log('Estoy dentro de else if');
   }
+  setLocalStorage();
 }
 
 shareButton.addEventListener('click', handleClickCreateBtn);
